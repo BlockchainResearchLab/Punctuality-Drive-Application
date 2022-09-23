5dc118b933cb28df77f0990aee645c81d0b49eec
Aadarsh Pandey <aadarsh2012014@akgec.ac.in>
2022-09-24 02:33:09 +0530

    Structure Ready

---
+++ lib/barcodeScanner.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class Scanner extends StatelessWidget {
+  const Scanner({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+import 'dart:ui';
+
 import 'package:flutter/material.dart';
 
 void main() {
@@ -12,42 +14,134 @@ class MainPage extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       home: Scaffold(
+        drawer: Drawer(
+          child: Container(
+            width: double.infinity,
+            decoration: BoxDecoration(
+              color: Colors.white,
+              image: DecorationImage(
+                // fit: BoxFit.fitHeight,
+                colorFilter: ColorFilter.mode(
+                    Colors.white.withOpacity(0.1), BlendMode.dstATop),
+                image: const AssetImage("images/akgeclogo.png"),
+              ),
+            ),
+            child: Column(
+              children: [
+                Column(
+                  children: [
+                    Center(
+                      child: Container(
+                        margin: const EdgeInsets.only(top: 40.0),
+                        width: 150,
+                        height: 150,
+                        decoration: const BoxDecoration(
+                          shape: BoxShape.circle,
+                          image: DecorationImage(
+                            fit: BoxFit.fill,
+                            image: AssetImage("images/akgeclogo.png"),
+                          ),
+                        ),
+                      ),
+                    ),
+                    const SizedBox(height: 5.0,),
+                    const Text(
+                      "UserName",
+                      style: TextStyle(
+                        fontSize: 30.0,
+                      ),
+                    ),
+                  ],
+                ),
+                const SizedBox(height: 15.0),
+                const ListTile(
+                  trailing: Icon(
+                    Icons.logout,
+                    size: 30.0,
+                  ),
+                  title: Text(
+                    "Log Out",
+                    style: TextStyle(
+                      fontSize: 20.0,
+                      color: Colors.black,
+                    ),
+                  ),
+                  onTap: null,
+                )
+              ],
+            ),
+          ),
+        ),
         appBar: AppBar(
           title: const Text("PUNCTUALITY DRIVE AKGEC"),
           centerTitle: true,
           backgroundColor: Colors.amberAccent,
           foregroundColor: Colors.blueGrey.shade900,
         ),
-        body: const Scanner(),
+        body: const ScannerPage(),
       ),
     );
   }
 }
 
-class Scanner extends StatelessWidget {
-  const Scanner({Key? key}) : super(key: key);
+class ScannerPage extends StatelessWidget {
+  const ScannerPage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Container(
-      color: Colors.white,
+      width: double.infinity,
+      decoration: BoxDecoration(
+        color: Colors.white,
+        image: DecorationImage(
+          // fit: BoxFit.fitHeight,
+          colorFilter: ColorFilter.mode(
+              Colors.white.withOpacity(0.1), BlendMode.dstATop),
+          image: const AssetImage("images/akgeclogo.png"),
+        ),
+      ),
       child: Column(
-        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        mainAxisAlignment: MainAxisAlignment.center,
         crossAxisAlignment: CrossAxisAlignment.center,
-        children: const [
+        children: [
           ElevatedButton(
-            onPressed: null,
-            style: ButtonStyle(
-              backgroundColor:
+            onPressed: () {},
+            style: ElevatedButton.styleFrom(
+              primary: Colors.amberAccent,
+              elevation: 2.5,
+              side: const BorderSide(
+                color: Colors.green,
+                style: BorderStyle.solid,
+              ),
             ),
-            child: Text("Button 1"),
+            child: const Text(
+              "SCAN CARD",
+              style: TextStyle(
+                color: Colors.black,
+                fontSize: 25.0,
+              ),
+            ),
           ),
-          SizedBox(
-            height: 50,
+          const SizedBox(
+            height: 25,
           ),
           ElevatedButton(
-            onPressed: null,
-            child: Text("Button 2"),
+            onPressed: () {},
+            style: ElevatedButton.styleFrom(
+              primary: Colors.amber,
+              elevation: 2.5,
+              side: const BorderSide(
+                color: Colors.red,
+                style: BorderStyle.solid,
+              ),
+            ),
+            child: const Text(
+              "LOGOUT",
+              style: TextStyle(
+                color: Colors.black,
+                fontSize: 15.0,
+              ),
+            ),
           ),
         ],
       ),
